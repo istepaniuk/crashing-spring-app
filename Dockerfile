e63dce9bc395e901dbd00fc6ff0edbcbd93d7e20
Ivan Stepaniuk <me@istepaniuk.com>
2021-09-14 23:47:50 +0200

    self-contained build in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
+FROM gradle:7.1-jdk16 AS BUILD_IMAGE
+
+WORKDIR /builder
+COPY . .
+RUN gradle --no-daemon clean build
+
 FROM amazoncorretto:16
 
-COPY ./build/libs/crashing-spring-app-0.0.1-SNAPSHOT.jar /app/app.jar
+COPY --from=BUILD_IMAGE /builder/build/libs/crashing-spring-app-0.0.1-SNAPSHOT.jar /app/app.jar
 
 EXPOSE 8080
-ENTRYPOINT [ "java", "-Xverify:none", "-jar", "/app/app.jar" ]
+ENTRYPOINT [ "java", "-jar", "/app/app.jar" ]
--- src/main/java/com/istepaniuk/crasher/CrashService.java
+++ src/main/java/com/istepaniuk/crasher/CrashService.java
@@ -7,16 +7,14 @@ public class CrashService {
     private Integer counter = 20;
 
     public ArrayList<Todo> getAllTodos() {
-        var todos = new ArrayList<>(Arrays.asList(
-                new Todo("this_backend_will", "crash miserably"),
-                new Todo("requests_left", counter.toString())
-        ));
-
         counter--;
         if (counter <= 0) {
             throw new RuntimeException("It finally crashed");
         }
 
-        return todos;
+        return new ArrayList<>(Arrays.asList(
+                new Todo("this_backend_will", "crash miserably"),
+                new Todo("requests_left", counter.toString())
+        ));
     }
 }
