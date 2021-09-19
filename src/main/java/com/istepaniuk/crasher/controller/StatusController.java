9b7a7f3c920a5f7151a80c681a32a07e8e2c1e04
Ivan Stepaniuk <me@istepaniuk.com>
2021-09-19 22:29:37 +0200

    adding hostname in response

--- src/main/java/com/istepaniuk/crasher/controller/StatusController.java
+++ src/main/java/com/istepaniuk/crasher/controller/StatusController.java
@@ -1,16 +1,25 @@
 package com.istepaniuk.crasher.controller;
 
 import com.istepaniuk.crasher.Status;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class StatusController {
     @GetMapping("/")
-    public Status getIndex(boolean shouldCrash) {
-        if(shouldCrash){
+    public List<Status> getIndex(boolean shouldCrash) throws UnknownHostException {
+        if (shouldCrash) {
             System.exit(1);
         }
 
-        return new Status("works", "fine");
+        return Arrays.asList(
+                new Status("works", "fine"),
+                new Status("host", InetAddress.getLocalHost().getHostName())
+        );
     }
 }
